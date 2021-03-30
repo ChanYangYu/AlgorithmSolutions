70e4c935eeea13c89eda93d47c7e788956585068
유찬양 <clsrnthd7@gmail.com>
2021-03-31 00:42:13 +0900

    9251번 LCS

--- BaekJoon/다이나믹 프로그래밍/9251번 LCS.cpp
+++ BaekJoon/다이나믹 프로그래밍/9251번 LCS.cpp
@@ -1,26 +1,39 @@
-#include<iostream>
 #include <bits/stdc++.h>
+#define MAX 15
 using namespace std;
-int dy[1001][1001]; 
-int main(int argc, char** argv)
-{
-	ios_base::sync_with_stdio(false);
-	char a[1001],b[1001];
-	cin>>a;
-	cin>>b;
+int cache[1001][1001];
+string A, B;
+
+int solve(int a, int b){
+	//기저 사례 
+	if(a >= A.size() || b >= B.size())
+		return 0;
 	
-	//a 배열 첫번째 알파벳 부터 검토 
-	for(int i = 1; i <= strlen(a); i++){
-		for(int j = 1; j <= strlen(b); j++){
-			//b배열에서 같은 알파벳이 있으면 i-1행 j-1열 dy값에 +1
-			if(a[i-1] == b[j-1])
-				dy[i][j] = dy[i-1][j-1] + 1;
-			//같은 알파벳이 아니면 
-			//새로운값이 갱신된 i행 이전j열 값과 이전 i행 j열 값 중 최고값 갱신 
-			else
-				dy[i][j] = max(dy[i-1][j], dy[i][j-1]);
-		}
-	}
-	cout<<dy[strlen(a)][strlen(b)];
+	int& ret = cache[a][b];
+	if(ret != -1)
+		return ret;
+	
+	//선택
+	int target = A[a];
+	
+	ret = 0;
+	for(int i = b; i < B.size(); i++)
+		if(target == B[i])
+			ret = max(ret, solve(a+1, i+1) + 1);
+	
+	ret = max(ret, solve(a+1, b));
+	
+	return ret;
+}
+int main() {
+	cin.tie(0);
+	//freopen("input.txt", "rt", stdin);
+	
+	cin>>A>>B;
+	
+	memset(cache, -1, sizeof(cache));
+	int ret = solve(0, 0);
+	
+	cout<<ret;
 	return 0;
 }
