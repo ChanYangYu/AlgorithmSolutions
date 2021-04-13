3753e31adbe43a9b1821720f15e680c475c20f58
유찬양 <clsrnthd7@gmail.com>
2021-04-13 19:09:36 +0900

    11726 2xn 타일링

---
+++ BaekJoon/다이나믹 프로그래밍/11726 2xn 타일링.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+int cache[1001];
+int solve(int pos){
+	int& ret = cache[pos];
+	
+	if(ret != -1)
+		return ret;
+	
+	//기저사례 
+	if(pos == n)
+		return ret = 1;
+	
+	//2x1 타일 
+	ret = solve(pos+1) % 10007;
+	
+	//2x2 타일 
+	if(pos + 2 <= n)
+		ret += solve(pos+2) % 10007;
+	
+	return ret % 10007;
+}
+
+int main(int argc, char** argv)
+{
+    cin.tie(0);
+    //freopen("input.txt","rt",stdin);
+    
+    cin>>n;
+    
+    memset(cache, -1, sizeof(cache));
+    
+    cout<<solve(0);
+    return 0;
+}
