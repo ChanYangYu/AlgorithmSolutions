f3e683ce92e84ff6bf4756f1fb3784b710c78d7d
유찬양 <clsrnthd7@gmail.com>
2021-06-09 21:51:35 +0900

    면접대비 leetcode 문제풀이

---
+++ LeetCode/Top 100 Linked Questions/21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,89 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// My Solution
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* res = NULL;
+        ListNode* head = NULL;
+        while(l1 != NULL && l2 != NULL){
+            if(l1 -> val < l2 -> val){
+                if(res == NULL){
+                    res = l1;
+                    head = res;
+                }
+                else{
+                    res->next = l1;
+                    res = res->next;
+                }
+                l1 = l1->next;
+            }
+            else{
+                if(res == NULL){
+                    res = l2;
+                    head = res;
+                }
+                else{
+                    res -> next = l2;
+                    res = res->next;
+                }
+                l2 = l2->next;
+            }
+            
+        }
+        
+        while(l1 != NULL){
+            if(res == NULL){
+                res = l1;
+                head = res;
+            }
+            else{
+                res -> next = l1;
+                res = res->next;
+            }
+            l1 = l1 -> next;
+        }
+        
+        while(l2 != NULL){
+            if(res == NULL){
+                res = l2;
+                head = res;
+            }
+            else{
+                res -> next = l2;
+                res = res->next;
+            }
+            l2 = l2 -> next;
+        }
+        
+        return head;
+    }
+};
+
+// Recursive Solution
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == NULL)
+            return l2;
+        if(l2 == NULL)
+            return l1;
+        if(l1->val < l2->val){
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+        else{
+            l2->next = mergeTwoLists(l2->next, l1);
+            return l2;
+        }
+    }
+};
