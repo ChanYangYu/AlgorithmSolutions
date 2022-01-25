94d88c9caa83910af3fc23364f67fc93d036c465
ChanYangYu <goodtss@soongsil.ac.kr>
2022-01-25 16:02:53 +0900

    파일 네이밍 수정

--- LeetCode/Top 100 Linked Questions/48. Rotate Image.js
+++ LeetCode/Top 100 Linked Questions/48. Rotate Image.js
@@ -1,41 +1,50 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
  */
-class Solution {
-public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* cur = head;
-        ListNode* prev = NULL;
-        ListNode* tmp = head;
-        int sz = 0;
+
+/*
+  풀이
+   - 테두리부터 원소하나씩 rotation 시키고,
+     내부로 들어가면서 (x, y를 대각선으로 이동) 내부 rotation
+*/
+
+const rotate = (matrix) => {
+    let n = matrix.length - 1;
+    let y = 0, x = 0;
+    let cur, next;
         
-        while(tmp != NULL){
-            sz++;
-            tmp = tmp->next;
+    while(n > 0){
+        let ry = 0, rx = n;
+        
+        for(let i = 0; i < n; i++){
+            // 첫번째 원소 바꿔야할 위치로 변경
+            next = matrix[y + ry][x + rx];
+            matrix[y + ry][x + rx] = matrix[y][x];
+            
+            // 두번째 원소 바꿔야할 위치로 변경
+            cur = next;
+            next = matrix[y + ry + rx][x + rx - ry];
+            matrix[y + ry + rx][x + rx - ry] = cur;
+            
+            // 세번째 원소 바꿔야할 위치로 변경
+            cur = next;
+            next = matrix[y + rx][x - ry];
+            matrix[y + rx][x - ry] = cur;
+            
+            
+            // 마지막 원소 위치 변경
+            matrix[y][x] = next;
+            
+            ry++;
+            rx--;
+            x++;
         }
-        int target = sz - n;
-        int idx = 0;
-        while(cur != NULL){
-            if(idx == target){
-                if(prev == NULL)
-                    head = head->next;
-                else if(cur->next == NULL)
-                    prev->next = NULL;
-                else
-                    prev->next = cur->next;
-                break;
-            }
-            idx++;
-            prev = cur;
-            cur = cur->next;
-        }
-        return head;
+        
+        // y, x 대각선으로 설정 ex) 0,0 -> 1,1 -> 2,2 ...
+        y++;
+        x = y;
+        n -= 2;
     }
+    
 };
